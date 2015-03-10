7ebf18f30d5b2ef430fa1da42177b1d90b412fab
Aldo Vargas <aldo@aldostools.org>
2015-03-10 08:11:19 -0400

    webMAN MOD 1.41.30
    - LITE Edition: Removed some flags to reduce the memory footprint (under
    192KB or 3 memory pages):
    * No auto-fix game
    * No loadprx.ps3
    * No option to change BD/DVD region

--- flags/flags_lite.h
+++ flags/flags_lite.h
@@ -10,15 +10,15 @@
 //#define LITE_EDITION	1	// no ps3netsrv support, smaller memory footprint
 
 //// FEATURES ////
-#define FIX_GAME		1	// Auto-Fix game
+//#define FIX_GAME		1	// Auto-Fix game
 #define EXT_GDATA		1	// /extgd.ps3?status /extgd.ps3?enable /extgd.ps3?disable (external gameDATA)
 //#define COPY_PS3		1	// /copy.ps3/<path>
 //#define WEB_CHAT		1	// /chat.ps3
 //#define DEBUG_MEM		1	// /dump.ps3 / peek.lv1 / poke.lv1 / find.lv1 / peek.lv2 / poke.lv2 / find.lv2
 //#define VIDEO_REC		1	// /videorec.ps3  start/stop video recording (in-game only)
-#define LOAD_PRX		1	// /loadprx.ps3?slot=n&prx=path/file.sprx  (load prx)
+//#define LOAD_PRX		1	// /loadprx.ps3?slot=n&prx=path/file.sprx  (load prx)
 #define FAKEISO 		1	// support .ntfs[BDFILE] (fake ISO)
-#define BDVD_REGION		1	// change BD/DVD region
+//#define BDVD_REGION	1	// change BD/DVD region
 
 //// TEST FEATURES ////
 //#define PS2_DISC		1	// uncomment to support /mount.ps2 (mount ps2 game folder as /dev_ps2disc)
